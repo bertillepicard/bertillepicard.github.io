bb3ecb7d6eb77ae0555e4646510670d5c69bcd28
Bbertt <picard.bertille@gmail.com>
2022-07-07 21:40:41 +0200

    update teaching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 const colors = require('tailwindcss/colors')
 const defautTheme = require('tailwindcss/defaultTheme')
 
-module.exports = {
+module.exports = {  
   mode: 'jit',
   purge: [
     './*.html'
   ],
   darkMode: 'class',
-  content: ["./index.html"],
+  content: ["./index.html", './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js', "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       ...defautTheme.screens,
@@ -21,11 +21,19 @@ module.exports = {
         'fondcarddark': '#2b2d2f',
         'darkshadowcard': '#47494b',
         'collink': 'var(--collink)',
+        'colhover': 'var(--colhover)',
       }
+    },
+  },
+  variants: {
+    extend: {
+      visibility: ["group-hover"],
     },
   },
   corePlugins: {
   },
   plugins: [
-  ],
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin')
+  ]
 }
